f20723133397ca2e338fb5f15603ba7a17ea6e8f
Alex <me@alexb.ninja>
2014-10-15 15:42:50 -0700

    remove awesomethings in scopes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('websiteApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
